cd1a2be1a536888514f9d5e558c8b5a22c399277
Marcus <marcus.armstrong42@gmail.com>
2023-09-11 15:48:39 -0500

    Further along

---
+++ hash_practice/target/debug/deps/hash_practice-cd0052fd53aa4456.d
@@ -0,0 +1,5 @@
+/home/marcus/Projects/RustBook/hash_practice/target/debug/deps/hash_practice-cd0052fd53aa4456: src/main.rs
+
+/home/marcus/Projects/RustBook/hash_practice/target/debug/deps/hash_practice-cd0052fd53aa4456.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/deps/variables-5b3c7708b8d51a36.d
@@ -0,0 +1,5 @@
+/home/marcus/Projects/RustBook/variables/target/debug/deps/variables-5b3c7708b8d51a36: src/main.rs
+
+/home/marcus/Projects/RustBook/variables/target/debug/deps/variables-5b3c7708b8d51a36.d: src/main.rs
+
+src/main.rs:
