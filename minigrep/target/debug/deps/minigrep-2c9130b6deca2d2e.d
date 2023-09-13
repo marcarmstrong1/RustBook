fcb2598c9af0c665b02d54511989ec89fb67ecd0
Marcus <marcus.armstrong42@gmail.com>
2023-09-13 13:20:04 -0500

    Further along

---
+++ minigrep/target/debug/deps/minigrep-2c9130b6deca2d2e.d
@@ -0,0 +1,5 @@
+/home/marcus/Projects/RustBook/minigrep/target/debug/deps/minigrep-2c9130b6deca2d2e: src/main.rs
+
+/home/marcus/Projects/RustBook/minigrep/target/debug/deps/minigrep-2c9130b6deca2d2e.d: src/main.rs
+
+src/main.rs:
